c3257a051f89544b6c2987dee56422e64345393e
Cho-Joon-Hee <ehdro64@gmail.com>
2021-01-27 09:42:26 +0900

    ㅅㅂ 뭔질 모르겠네

--- front/src/Yang/About.js
+++ front/src/Yang/About.js
@@ -1,11 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Chart from 'chart.js';
 
-class About extends Component{
+class About extends Component {
     render() {
-        return(
+        const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+            type: "pie",
+            data: {
+              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+              datasets: [
+                {
+                  label: "# of Votes",
+                  data: [12, 19, 3, 5, 2, 3],
+                  backgroundColor: [
+                    "Red",
+                    "Blue",
+                    "Yellow",
+                    "Green",
+                    "Purple",
+                    "Orange"
+                  ],
+                  borderColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                  borderWidth: 1
+                }
+              ]
+            }
+          });
+      
+        return (
             <div>
                 <h2>준희 공부</h2>
-            </div>
+                <h1>TEST</h1>
+                <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+
+
         );
     }
 }
