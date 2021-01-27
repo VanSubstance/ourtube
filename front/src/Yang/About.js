3979b3de4c909c0437917c12e10d21d199ac3b0d
Cho-Joon-Hee <ehdro64@gmail.com>
2021-01-27 10:07:36 +0900

    바 그래프 생성
    바 그래프 생성

--- front/src/Yang/About.js
+++ front/src/Yang/About.js
@@ -1,37 +1,50 @@
 import React, { Component } from 'react';
-import Chart from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 class About extends Component {
     render() {
-        const ctx = document.getElementById('myChart');
-        new Chart(ctx, {
-            type: "pie",
-            data: {
-              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-              datasets: [
-                {
-                  label: "# of Votes",
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                    "Red",
-                    "Blue",
-                    "Yellow",
-                    "Green",
-                    "Purple",
-                    "Orange"
-                  ],
-                  borderColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                  borderWidth: 1
-                }
-              ]
-            }
-          });
-      
         return (
             <div>
                 <h2>준희 공부</h2>
-                <h1>TEST</h1>
-                <canvas id="myChart" width="400" height="400"></canvas>
+                <Bar
+                    data = {{
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: 'test',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    }}
+                    height = {400}
+                    width = {600}
+                    options={{
+                        maintainAspectRatio: false,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }}
+                />
+                
         </div>
 
 
