4218e2c5d9ea4ae60e97bc9e628340337a4bc631
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2021-02-03 17:07:06 +0900

    Add components for graph
    AlltimeMainPage
    TrendMainPage

--- front/src/Design/AlltimeMainPage.js
+++ front/src/Design/AlltimeMainPage.js
@@ -3,13 +3,43 @@ import React, { Component } from 'react';
 class AlltimeMainPage extends Component {
     
     state = {
+        chart: {
+            width: "200px",
+            height: "100px",
+            background: "#000000",
+            color: "#FFFFFF",
+            margin: "100 100 100 100",
+        }
     }
     render () {
         return (
             console.log("SearchPage opened."),
             <div>
-                <p>상세 페이지</p>
-                <p>{this.props.searchType}</p>
+                <p>{this.props.searchType} 페이지</p>
+                <div
+                    style={this.state.chart}>
+                    <p> 바 그래프: 좋아요 싫어요 비율 </p>
+                </div>
+
+                <div
+                    style={this.state.chart}>
+                    <p> 선 그래프: 월별 순위 변동 </p>
+                </div>
+                
+                <div
+                    style={this.state.chart}>
+                    <p> 선 그래프: 키워드 관련 동영상 신규 조회수 </p>
+                </div>
+                
+                <div
+                    style={this.state.chart}>
+                    <p> 선 그래프: 키워드 관련 신규 동영상 개수 </p>
+                </div>
+                
+                <div
+                    style={this.state.chart}>
+                    <p> 선 그래프: 키워드 관련 동영상 신규 조회수 </p>
+                </div>
             </div>
         );
     }
