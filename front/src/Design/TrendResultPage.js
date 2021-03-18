647cb4fb7f693c63cb4ceef802741d7eef6e61ff
kang123077 <glolycon@gmail.com>
2021-03-18 23:15:01 +0900

    [추가] TrendResultPage 가운데 정렬, ctgr, badge 추가

--- front/src/Design/TrendResultPage.js
+++ front/src/Design/TrendResultPage.js
@@ -1,41 +1,90 @@
 import React, { Component } from 'react';
 import './Styles.css';
+import './Css/TrendResultPage.css';
 import { ListFont, TrendChip } from './Comps';
 
 class TrendResultPage extends Component {
     render() {
         return (
-            <div>
+            <div
+                className="trp_mainContainer">
+                <div id="header">
+                    <div
+                        className="trp_BannerBox">
+                        <a
+                            className="trp_BannerA"
+                            href="http://localhost:3012/">
+                            <img
+                                className="trp_BannerImage"
+                                src="/Ex/ourtubeLogoWhite.PNG">
+                            </img>
+                        </a>
+                    </div>
+                    <div
+                        className="trp_InfoBar">
+                            <div
+                                className="trp_CtgrChip">
+                                    RPG
+                            </div>
+                            <div
+                                className="trp_CtgrChip">
+                                    AOS
+                            </div>
+                            <div
+                                className="trp_CtgrChip">
+                                    액션
+                            </div>
+                            <div
+                                className="trp_BadgeBar">
+                                    <div
+                                        className="trp_BadgeChip">
+                                        이 주의 조회수 1위
+                                    </div>
+                                    <div
+                                        className="trp_BadgeChip">
+                                        이 주의 신규 동영상 1위
+                                    </div>
+                                    <div
+                                        className="trp_BadgeChip">
+                                        이 주의 최고 RPG 게임
+                                    </div>
+                                    <div
+                                        className="trp_BadgeChip">
+                                        양승혁의 골짜기
+                                    </div>
+                            </div>
+                    </div>
+                </div>
                 {/* //배경 및 그라데이션  */}
                 <div id="scollWallpaper_">
-                    <img id="andy-holmes-rCbdp8VCYhQ-unspla1" src="./Ex/andy-holmes-rCbdp8VCYhQ-unspla.png"
-                        srcset="./andy-holmes-rCbdp8VCYhQ-unspla.png 1x, ./andy-holmes-rCbdp8VCYhQ-unspla@2x.png 2x" />
+                    <img
+                        id="andy-holmes-rCbdp8VCYhQ-unspla1"
+                        src="./Ex/andy-holmes-rCbdp8VCYhQ-unspla.png"
+                        srcSet="./Ex/andy-holmes-rCbdp8VCYhQ-unspla.png 1x, ./Ex/andy-holmes-rCbdp8VCYhQ-unspla@2x.png 2x"
+                    />
                 </div>
                 <svg className="scollGradient" viewBox="0 0 1920 1500">
-                    <linearGradient id="_137_bn" spreadMethod="pad" x1="0.952" x2="0.071" y1="0.067" y2="0.886">
-                        <stop offset="0" stop-color="#f60" stop-opacity="1"></stop>
-                        <stop offset="0.1673" stop-color="#cf8845" stop-opacity="1"></stop>
-                        <stop offset="0.3389" stop-color="#b79967" stop-opacity="1"></stop>
-                        <stop offset="0.4937" stop-color="#b4838b" stop-opacity="1"></stop>
-                        <stop offset="0.6735" stop-color="#b16ab4" stop-opacity="1"></stop>
-                        <stop offset="0.7991" stop-color="#7c84b9" stop-opacity="1"></stop>
-                        <stop offset="1" stop-color="#28adc2" stop-opacity="1"></stop>
+                    <linearGradient
+                        id="_137_bn"
+                        spreadMethod="pad"
+                        x1="0.952"
+                        x2="0.071"
+                        y1="0.067"
+                        y2="0.886"
+                    >
+                        <stop offset="0" stopColor="#f60" stopOpacity="1"></stop>
+                        <stop offset="0.1673" stopColor="#cf8845" stopOpacity="1"></stop>
+                        <stop offset="0.3389" stopColor="#b79967" stopOpacity="1"></stop>
+                        <stop offset="0.4937" stopColor="#b4838b" stopOpacity="1"></stop>
+                        <stop offset="0.6735" stopColor="#b16ab4" stopOpacity="1"></stop>
+                        <stop offset="0.7991" stopColor="#7c84b9" stopOpacity="1"></stop>
+                        <stop offset="1" stopColor="#28adc2" stopOpacity="1"></stop>
                     </linearGradient>
-                    <path id="Gradient" d="M 0 0 L 1920 0 L 1920 1500 L 0 1500 L 0 0 Z">
-                    </path>
+                    <path
+                        id="Gradient"
+                        d="M 0 0 L 1920 0 L 1920 1500 L 0 1500 L 0 0 Z"
+                    ></path>
                 </svg>
-                <div id="scollOURTUBE">
-                    <span>OURTUBE</span>
-                </div>
-                <svg className="ReSwipeBox">
-                    <rect id="ReSwipeBox" rx="0" ry="0" x="0" y="0" width="934" height="41">
-                    </rect>
-                </svg>
-                <div id="swiperFont">
-                    <span>이 주의 조회수 1위</span>
-                    <span>이 주의 조회수 1위</span>
-                    <span>이 주의 조회수 1위</span>
-                </div>
                 {/* <TrendChip></TrendChip> */}
                 <svg className="listTop">
                     <rect id="listTop" rx="0" ry="0" x="0" y="0" width="923" height="41">
